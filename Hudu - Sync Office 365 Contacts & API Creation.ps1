4b4915e9195f4ee79af70758c2a1ba4ec991889c
unknown <andrew1994android@gmail.com>
2021-07-15 18:02:29 +0100

    Added API Creation and secret creation

--- Hudu - Sync Office 365 Contacts & API Creation.ps1
+++ Hudu - Sync Office 365 Contacts & API Creation.ps1
@@ -7,7 +7,29 @@ if (Get-Module -Name AzureAd) {
         Install-Module AzureAD -Force -Confirm:$false
         Import-Module AzureAD 
         }
-            Connect-AzureAD
+            
+Connect-AzureAD
 
-
-
+#Create AzureAD App for Hudu Sync
+$AppName = "Hudu Sync"
+    if(!($myApp = Get-AzureADApplication -Filter "DisplayName eq '$($appName)'"  -ErrorAction SilentlyContinue))
+    {
+        $myApp = New-AzureADApplication -DisplayName $appName  
+# Application Password Credentials (ClientSecret)
+$startDate = Get-Date
+$endDate = $startDate.AddYears(5)
+$aadAppKeyPwd = New-AzureADApplicationPasswordCredential -ObjectId $AADApplication.ObjectId -CustomKeyIdentifier "Primary" -StartDate $startDate -EndDate $endDate
+$aadAppKeyPwd
+$AppDetailsOutput = "$AADApplicationName Details:
+=========================================================
+Application Name: 	$appName
+Application Id:   	$($myApp.AppId)
+Secret Key:       	$($PasswordCredential.Value)
+"
+	Write-Host
+	Write-Host $AppDetailsOutput
+}
+else {
+	        Write-Host
+	        Write-Host -f Yellow Azure AD Application $appName already exists.
+}
